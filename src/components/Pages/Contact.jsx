4fffd135e70cadea9d328755a33d72a1816044ea
gblessylva <gblessylva@gmail.com>
2020-02-18 12:24:06 +0100

    Forms added without library

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,7 +1,38 @@
 import React, {Component} from 'react'
 import Header from '../Common/Header'
-class Contact extends Component{
 
+import Fields from '../Common/Fields'
+
+
+const fields = {
+        sections: [
+            [
+                {name:'name', elementName:'input',  type:'text', id:'name', placeholder: 'Your Name' },
+                {name:'email', elementName:'input',  type:'email', id:'name', placeholder: 'Your Email Address' },
+                {name:'phone', elementName:'input',  type:'tel', id:'name', placeholder: 'Your Phone Number' }
+            ],
+            
+            [
+             {name:'message', elementName:'textarea',  id:'name', placeholder: 'Your Message' }
+            ]
+        ]
+}
+   
+
+class Contact extends Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            name:'',
+            email:'',
+            phone:'',
+            message:''
+        }
+    }
+    submitForm=(e)=>{
+        alert('Form Sent')
+    }
     render(){
         return(
             <div>
@@ -13,7 +44,8 @@ class Contact extends Component{
                             // showButton={true}
                             image='/img/contact/contact_us.jpg'
                  />
-                 <section className="page-section" id="contact">
+                
+                <section className="page-section" id="contact">
                     <div className="container">
                     <div className="row">
                         <div className="col-lg-12 text-center">
@@ -23,39 +55,33 @@ class Contact extends Component{
                     </div>
                     <div className="row">
                         <div className="col-lg-12">
-                        <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                        <form id="contactForm" name="sentMessage" noValidate="noValidate">
                             <div className="row">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
-                                    <p className="help-block text-danger" />
-                                </div>
-                                <div className="form-group">
-                                <input className="form-control" id="email" type="email" placeholder="Your Email *" 
-                                    required="required" 
-                                    data-validation-required-message="Please enter your email address."/>
-                                <p className="help-block text-danger" />
-                                </div>
-                                <div className="form-group">
-                                <input className="form-control" id="phone" 
-                                type="tel" placeholder="Your Phone *" required="required" 
-                                data-validation-required-message="Please enter your phone number." />
-                                <p className="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                <textarea className="form-control" id="message" placeholder="Your Message *" 
-                                required="required" data-validation-required-message="Please enter a message." />
-
-                                
-                                <p className="help-block text-danger"></p>
-                                </div>
-                            </div>
+                                {fields.sections.map((section, sectionIndex) =>{
+                                    console.log('rendering ', sectionIndex, 'with', section )
+                                   return(
+                                       
+                                       <div className="col-md-6">
+                                           {section.map((field, i) =>{
+                                               return <Fields {...field} 
+                                               key={i}
+                                                    value={this.state[field.name]}
+                                                    onChange={e=>this.setState({[field.name]: e.target.value})}
+                                               />
+                                              
+                                           })}
+                                       </div>
+                                   ) 
+                                })}
                             <div className="clearfix"></div>
                             <div className="col-lg-12 text-center">
                                 <div id="success"></div>
-                                <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+                                <button 
+                                    id="sendMessageButton" 
+                                    className="btn btn-primary btn-xl text-uppercase" 
+                                    type="submit"
+                                    onClick={e=>this.submitForm()}
+                                    >Send Message</button>
                             </div>
                             </div>
                         </form>
