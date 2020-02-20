684f4bf6e80c2069b9916f465c8d4ecce4dec2f0
gblessylva <gblessylva@gmail.com>
2020-02-20 13:41:39 +0100

    loopback added

---
+++ portfolio/server/server.js
@@ -0,0 +1,55 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-workspace
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+const loopback = require('loopback');
+const boot = require('loopback-boot');
+
+const app = module.exports = loopback();
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    const baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      const explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+  });
+};
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
+
+console.log(Object.keys(app.models));
+
+app.models.User.afterRemote('create', (ctx, user, next)=>{
+  console.log('user name is', user);
+  app.models.Profile.create({
+    userName: user.username,
+    createdAt: new Date(),
+    userId: user.id,
+  },
+    (err, result)=>{
+      if (!err && result){
+        console.log('new user', result);
+      } else {
+        console.log(err);
+      }
+      next();
+    }
+
+  );
+});
